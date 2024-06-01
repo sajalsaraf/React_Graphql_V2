c8490e638aac500d0369e53d65f2cb112ccda2af
sajalsaraf <sajalsaraf161203@gmail.com>
2024-06-01 20:06:20 +0530

    delete query

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -2,14 +2,24 @@ import React, { Component } from 'react';
 import gql from 'graphql-tag';
 import { graphql } from 'react-apollo';
 import { Link } from 'react-router';
-import query from '../queries/FetchSongs.js';
+import query from '../queries/fetchSongs';
 
 class SongList extends Component {
+  onSongDelete(id) {
+    this.props.mutate({ variables: { id } });
+  }
+
   renderSongs() {
     return this.props.data.songs.map(song => {
       return (
         <li key={song.id} className="collection-item">
           {song.title}
+          <i
+            className="material-icons"
+            onClick={() => this.onSongDelete(song.id)}
+          >
+            delete
+          </i>
         </li>
       );
     });
@@ -34,4 +44,14 @@ class SongList extends Component {
   }
 }
 
-export default graphql(query)(SongList);
+const mutation = gql`
+  mutation DeleteSong($id: ID) {
+    deleteSong(id: $id) {
+      id
+    }
+  }
+`;
+
+export default graphql(mutation)(
+  graphql(query)(SongList)
+);
