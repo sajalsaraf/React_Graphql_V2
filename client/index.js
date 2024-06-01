e1f9a7b9036c24b2205ef5e6e2c5e5bf7a08a71e
sajalsaraf <sajalsaraf161203@gmail.com>
2024-06-01 15:55:09 +0530

    create mutations

--- client/index.js
+++ client/index.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, hashHistory, IndexRoute } from 'react-router';
 import ApolloClient from 'apollo-client';
 import { ApolloProvider } from 'react-apollo';
+
+import App from './components/App';
+import SongList from './components/SongList';
 
 const client = new ApolloClient({});
 
 const Root = () => {
   return (
     <ApolloProvider client={client}>
-      <div>Lyrical</div>
+      <Router history={hashHistory}>
+        <Route path="/" component={App}>
+          <IndexRoute component={SongList} />
+        </Route>
+      </Router>
     </ApolloProvider>
   );
 };
