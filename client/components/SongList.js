f5b733c41cde0f0c906408f0c7322e77e1d8b7f7
sajalsaraf <sajalsaraf161203@gmail.com>
2024-06-02 15:38:11 +0530

    datailing about page

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -6,17 +6,20 @@ import query from '../queries/fetchSongs';
 
 class SongList extends Component {
   onSongDelete(id) {
-    this.props.mutate({ variables: { id } });
+    this.props.mutate({ variables: { id } })
+      .then(() => this.props.data.refetch());
   }
 
   renderSongs() {
-    return this.props.data.songs.map(song => {
+    return this.props.data.songs.map(({ id, title }) => {
       return (
-        <li key={song.id} className="collection-item">
-          {song.title}
+        <li key={id} className="collection-item">
+          <Link to={`/songs/${id}`}>
+            {title}
+          </Link>
           <i
             className="material-icons"
-            onClick={() => this.onSongDelete(song.id)}
+            onClick={() => this.onSongDelete(id)}
           >
             delete
           </i>
